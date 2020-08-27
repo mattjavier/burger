e8157d711e6f1a94238100239b8db3456c05c1d7
Matt Javier <matthewrjavier@gmail.com>
2020-08-26 18:38:03 -0700

    edit sql, begin hbs files

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name, devoured)
 VALUES
 ('Big Kahuna Burger', false),
-('Double Double Cheeseburger', false)
+('Double Double Cheeseburger', false),
 ('Big Mac', false);
