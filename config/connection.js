44490d0f37f3a79d4069291dacbe85de730cc8e4
Matt Javier <matthewrjavier@gmail.com>
2020-08-26 19:09:16 -0700

    added public/ files, finsihed orm and connection

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,3 @@
 const mysql = require('mysql2')
 
-module.exports = mysql.createConnection('mysql://root:JaJ012566m@localhost/burgers_db')
+module.exports = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:JaJ012566m@localhost/burgers_db')
--- config/orm.js
+++ config/orm.js
@@ -2,16 +2,25 @@ const db = require('./connection.js')
 
 const orm = {}
 
-orm.selectAll = () => {
-
+orm.selectAll = (table, cb) => {
+  db.query(`SELECT * FROM ${table}`, (err, data) => {
+    if (err) { console.log(err) }
+    cb(data)
+  })
 } 
 
-orm.insertOne = () => {
-
+orm.insertOne = (table, data, cb) => {
+  db.query(`INSERT INTO ${table} SET ?`, data, (err, fields) => {
+    if (err) { console.log(err) }
+    cb(data)
+  })
 }
 
-orm.updateOne = () => {
-
+orm.updateOne = (table, updates, where, cb) => {
+  db.query(`UPDATE ${table} SET ? WHERE ?`, [updates, where], err => {
+    if (err) { console.log(errr) }
+    cb()
+  })
 }
 
 module.exports = orm
