f3ab3b77fc7012ff6316dce72f6094dc06cb5c0e
Matt Javier <matthewrjavier@gmail.com>
2020-08-26 19:14:46 -0700

    completed burger model

--- models/burger.js
+++ models/burger.js
@@ -2,5 +2,16 @@ const orm = require('../config/orm.js')
 
 const model = {}
 
+model.read = cb => {
+  orm.getAll('burgers', burgers => cb(burgers))
+}
+
+model.create = (burger, cb) => {
+  orm.insertOne('burgers', burger, id => cb(id))
+}
+
+model.update = (updates, where, cb) => {
+  orm.updateOne('burgers', updates, where, () => cb())
+}
 
 module.exports = model
